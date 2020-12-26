31e4962e03746b2573e0b08565a6b5c3468d40d9
Michael Pollind <mpollind@gmail.com>
2020-12-26 13:28:38 -0800

    fix implementation

--- src/main/java/org/terasology/minerals/generation/OreFacet.java
+++ src/main/java/org/terasology/minerals/generation/OreFacet.java
@@ -1,6 +1,7 @@
 
 package org.terasology.minerals.generation;
 
+import org.terasology.world.block.BlockRegion;
 import org.terasology.world.block.BlockRegionc;
 import org.terasology.world.generation.Border3D;
 import org.terasology.world.generation.facets.base.BaseFieldFacet3D;
@@ -8,132 +9,132 @@ import org.terasology.world.generation.facets.base.BaseFieldFacet3D;
 public class OreFacet extends BaseFieldFacet3D {
 
     public OreFacet(BlockRegionc targetRegion, Border3D border) {
-        super(targetRegion, border);
+        super(new BlockRegion(targetRegion), border);
     }
-    
+
     public class Acanthite extends OreFacet{
         public Acanthite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Bauxite extends OreFacet{
         public Bauxite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class BituminousCoal extends OreFacet{
         public BituminousCoal(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Cassiterite extends OreFacet{
         public Cassiterite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Chalcopyrite extends OreFacet{
         public Chalcopyrite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Chlorargyrite extends OreFacet{
         public Chlorargyrite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Chrysocolla extends OreFacet{
         public Chrysocolla(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Coal extends OreFacet{
         public Coal(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Goethite extends OreFacet{
         public Goethite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Hematite extends OreFacet{
         public Hematite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Limonite extends OreFacet{
         public Limonite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Magnetite extends OreFacet{
         public Magnetite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class NativeCopper extends OreFacet{
         public NativeCopper(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class NativeElectrum extends OreFacet{
         public NativeElectrum(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class NativeGold extends OreFacet{
         public NativeGold(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class NativeSilver extends OreFacet{
         public NativeSilver(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Pyrargyrite extends OreFacet{
         public Pyrargyrite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Rutile extends OreFacet{
         public Rutile(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Siderite extends OreFacet{
         public Siderite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Stibnite extends OreFacet{
         public Stibnite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
-    
+
     public class Titanite extends OreFacet{
         public Titanite(BlockRegionc targetRegion, Border3D border) {
             super(targetRegion, border);
-        }   
+        }
     }
 }
